b649dbc53efaca9834822aaf3b59b2a51cc30e3a
Zwei <89715693+derzwei@users.noreply.github.com>
2021-09-01 20:30:28 -0400

    Update rc.lua

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -279,7 +279,7 @@ globalkeys = gears.table.join(
     
     -- Open google-chrome
     awful.key({ modkey }, "b", function ()
-    awful.util.spawn("google-chrome-stable") end,
+    awful.util.spawn("chromium --enable-features=WebUIDarkMode --force-dark-mode") end,
               {description = "open a browser", group = "zwei"}),
 
     awful.key({ modkey, "Control" }, "r", awesome.restart,
