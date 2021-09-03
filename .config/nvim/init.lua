f11ba33a1ff78394592b69c7aa4508fcf54767bf
derzwei <ramallomatiasar@gmail.com>
2021-09-03 11:20:13 -0300

    Changes

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,6 +1,7 @@
 require('lspconfig')
 require('compe')
 require('nvim-treesitter')
+--require('nvim-tree')
 require('lualine')
 require('config')
 require('plugins')
--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -19,6 +19,7 @@ require('packer').startup(function()
   use 'neovim/nvim-lspconfig'                                                           -- Collection of configurations for built-in LSP client
   use 'hrsh7th/nvim-compe'                                                              -- Autocompletion plugin
   use {'nvim-treesitter/nvim-treesitter', run = ':TSUpdate'}                            -- Treesitter configurations and abstraction layer for Neovim
+  use {'kyazdani42/nvim-tree.lua',requires = 'kyazdani42/nvim-web-devicons'}            -- A file explorer tree for neovim written in lua
   use {'hoob3rt/lualine.nvim', requires = {'kyazdani42/nvim-web-devicons', opt = true}} -- Statusline written in pure lua.
   use "lukas-reineke/indent-blankline.nvim"                                             -- Indent guides for Neovim
   use 'navarasu/onedark.nvim'                                                           -- One Dark Theme for Neovim
